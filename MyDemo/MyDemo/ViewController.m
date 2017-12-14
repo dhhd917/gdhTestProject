ba1b9127fe59a3c2e8b04c372fd76c64b8841c0a
gdh <2851729082@qq.com>
2017-12-14 11:31:46 +0800

    feat:初始化项目

---
+++ MyDemo/MyDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MyDemo
+//
+//  Created by gdh on 2017/12/14.
+//  Copyright © 2017年 gdh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+	NSLog(@"hello world");
+}
+
+@end
